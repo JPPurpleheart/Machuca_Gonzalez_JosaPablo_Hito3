4d1093df5e3727c20c491e880731657d28fbcf1b
José Pablo Machuca González <71435599+JPPurpleheart@users.noreply.github.com>
2022-04-26 02:16:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iglesia Cristiana Evangélica Horeb (San Pablo) - Inicio</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../index.css">
+</head>
+<body>
+    <div class="bg-primary" id="header">
+        <nav class="navbar navbar-light" id="menu-hamburguesa">
+            <ul style="list-style-type: none;" id="menu-sub-hamburguesa">
+                <li class="col-12" style="width: 168px;">
+                    <img src="./img/logo.png" class="col-12" id="logo" height="168px" alt="Logo Horeb San Pablo">
+                </li>
+                <li class="col-12" id="lista-hamburguesa">
+                    <div class="containwhiteuid">
+                        <a class="navbar-brand text-light" href="../">Iglesia Cristiana Evangélica Horeb (San Pablo)</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto" id="lista-hamburguesa">
+                                <li class="nav-item"><a class="nav-link active text-light" aria-current="page" href="../">Inicio</a></li>
+                                <li class="nav-item /*dropdown*/">
+                                <a class="nav-link dropdown-toggle text-light" data-toggle="dropdown" href="recursos.html" role="button" aria-haspopup="true" aria-expanded="false">Recursos</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="recursos.html#libreria">Librería Horeb</a>
+                                    <!-- <a class="dropdown-item" href="#">Boletines</a>
+                                    <a class="dropdown-item" href="#">Alabanzas</a>
+                                    <a class="dropdown-item" href="#">Predicaciones</a>
+                                    <a class="dropdown-item" href="#">Cursos en Vídeo</a> -->
+                                </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle text-light" data-toggle="dropdown" href="nuestros_ministerios.html" role="button" aria-haspopup="true" aria-expanded="false">Nuestros Ministerios</a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#estudios_biblicos">Estudios Bíblicos
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#intercesion">Intercesión</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#mision_cazalla">Misión Cazalla de la Sierra</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#despensa">Despensa y Ropero</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#culto_oracion">Cultos de Oración</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#reunion_jovenes">Reuniones de Jóvenes</a>
+                                        
+                                            <hr class="dropdown-divider">
+                                        
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#">Alabanza</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#">Escuela Dominical</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#">Investigadores de la Biblia</a>
+                                        <a class="dropdown-item" href="nuestros_ministerios.html#">Predicación de la Palabra</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item"><a class="nav-link text-light" aria-current="page" href="localizacion.html">Localización y contacto</a></li>
+                                <li class="nav-item"><a class="nav-link text-light" aria-current="page" href="login.html">Login</a></li>
+                                <form class="d-flex">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-light text-light" type="submit">Search</button>
+                                </form>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <nav class="navbar-md navbar-light" id="menu-expandido">
+            <ul style="list-style-type: none;" class="col-12 d-inline-flex" id="menu-sub-expandido">
+                <li style="width: 168px;">
+                    <img src="./img/logo.png" class="col-12" height="168px" alt="Logo Horeb San Pablo">
+                </li>
+                <li class="col-11" id="lista-expandida">
+                    <ul class="nav nav-tabs" id="lista-expandida">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="../">Inicio</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-light" data-toggle="dropdown" href="recursos.html" role="button" aria-haspopup="true" aria-expanded="false">Recursos</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="recursos.html#libreria">Librería Horeb</a>
+                            <!-- <a class="dropdown-item" href="#">Boletines</a>
+                            <a class="dropdown-item" href="#">Alabanzas</a>
+                            <a class="dropdown-item" href="#">Predicaciones</a>
+                            <a class="dropdown-item" href="#">Cursos en Vídeo</a> -->
+                        </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light" data-toggle="dropdown" href="nuestros_ministerios.html" role="button" aria-haspopup="true" aria-expanded="false">Nuestros Ministerios</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="nuestros_ministerios.html#estudios_biblicos">Estudios Bíblicos
+                                <a class="dropdown-item" href="nuestros_ministerios.html#intercesion">Intercesión</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#mision_cazalla">Misión Cazalla de la Sierra</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#despensa">Despensa y Ropero</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#culto_oracion">Cultos de Oración</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#reunion_jovenes">Reuniones de Jóvenes</a>
+                                
+                                    <hr class="dropdown-divider">
+                                
+                                <a class="dropdown-item" href="nuestros_ministerios.html#">Alabanza</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#">Escuela Dominical</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#">Investigadores de la Biblia</a>
+                                <a class="dropdown-item" href="nuestros_ministerios.html#">Predicación de la Palabra</a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link text-light" aria-current="page" href="localizacion.html">Localización y
+                                contacto</a></li>
+                        <li class="nav-item"><a class="nav-link bg-light text-primary" aria-current="page" href="login.html">Login</a></li>
+                        <form class="d-flex align-items-center">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-light text-light" type="submit">Search</button>
+                        </form>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./img/photo-carrusel-1.jpg" class="d-block w-100" alt="Isaías 43:1-2">
+          </div>
+          <div class="carousel-item">
+            <img src="./img/photo-carrusel-2.jpg" class="d-block w-100" alt="Jeremías 32:27">
+          </div>
+          <div class="carousel-item">
+            <img src="./img/photo-carrusel-3.jpg" class="d-block w-100" alt="Marcos 11:24">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="jumbotron text-center">
+        <h1 class="display-4">¡Llevando el Evangelio al mundo!</h1>
+        <h5 class="lead">"Lo primero que les enseñé fue lo mismo que yo aprendí: que Cristo murió en lugar de nosotros, que éramos
+            pecadores. Tal como lo enseña la Biblia, fue sepultado y, después de tres días, Dios lo resucitó." - I
+            Corintios 15:3-4 Traducción en Lenguaje Actual</h5>
+        <hr class="my-4">
+        <h3>Iglesia Cristiana Evangélica Horeb es una comunidad cristiana en el Polígono San Pablo sita en la calle
+            Doctor Laffon Soto s/n frente al Polideportivo San Pablo. Como comunidad cristiana, buscamos llevar la
+            palabra de Dios a todas las personas como está escrito: "Ustedes vayan y hagan más discípulos míos en todos
+            los países de la tierra. Bautícenlos en el nombre del Padre, del Hijo y del Espíritu Santo. Enséñenles a
+            obedecer todo lo que yo les he enseñado. Yo estaré siempre con ustedes, hasta el fin del mundo." - Mateo
+            28:19-20 Traducción en Lenguaje Actual, adicionalmente la iglesia cuenta con un ministerio de Despensa y
+            Ropero donde se apoya a los más necesitados con comida y ropa para todos aquellos que verdaderamente lo
+            necesiten y una reunión semanal los sábados exclusiva para los jóvenes con hermanos y hermanas de Nueva Sión
+            y Manantial de Vida entre otras Iglesias de la ciudad.</h3>
+        <a class="btn btn-primary btn-lg" href="localizacion.html#card" role="button">Contáctanos</a>
+        <a class="btn btn-primary btn-lg" href="recursos.html#predicaciones" role="button">Predicaciones de Colosenses</a>
+      </div>
+      <hr>
+    <footer>
+        <h2 class="col-12 text-center">Testimonios</h2>
+        <div class="col-12 d-block d-md-inline-flex justify-content-md-around" id="center-less-md">
+            <div class="card align-items-center text-center" style="width: 18rem;">
+                <img src="http://via.placeholder.com/460" class="card-img-top" alt="photo 1">
+                <div class="card-body">
+                    <h5 class="card-title">Álvaro Martínez, 25 años</h5>
+                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus molestiae, ab quae omnis quia
+                    tenetur consequatur, recusandae voluptatem dolorem nulla sint tempora ducimus numquam? Et eos ipsum
+                    dolores optio excepturi?</p>
+                    <a href="localizacion.html#card" class="btn btn-primary">Solicita un Evangelio</a>
+                </div>
+            </div>
+            <div class="card align-items-center text-center" style="width: 18rem;">
+                <img src="http://via.placeholder.com/460" class="card-img-top" alt="photo 1">
+                <div class="card-body">
+                    <h5 class="card-title">Josefa Canela, 45 años</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nulla dicta nesciunt nemo assumenda!
+                    Mollitia fugit sequi tenetur nobis repudiandae earum natus, quibusdam, repellendus iste numquam aliquam
+                    commodi voluptate ratione!</p>
+                    <a href="localizacion.html#card" class="btn btn-primary">Solicita un Evangelio</a>
+                </div>
+            </div>
+            <div class="card align-items-center text-center" style="width: 18rem;">
+                <img src="http://via.placeholder.com/460" class="card-img-top" alt="photo 1">
+                <div class="card-body">
+                    <h5 class="card-title">Sebastián Menéndez, 79 años</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates deserunt quis nihil, dignissimos ex
+                    totam eius laboriosam dolore distinctio repellat fugit ut facilis numquam aperiam quasi cumque
+                    perferendis fuga doloremque.</p>
+                    <a href="localizacion.html#card" class="btn btn-primary">Solicita un Evangelio</a>
+                </div>
+            </div>
+            <div class="card align-items-center text-center" style="width: 18rem;">
+                <img src="http://via.placeholder.com/460" class="card-img-top" alt="photo 1">
+                <div class="card-body">
+                    <h5 class="card-title">Rosa Sánchez, 32 años</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, praesentium esse non beatae dolor fugiat officia quisquam iure. Soluta, maiores! Nihil consequuntur aperiam nobis mollitia eaque, odio recusandae corporis sit!</p>
+                    <a href="localizacion.html#card" class="btn btn-primary">Solicita un Evangelio</a>
+                </div>
+            </div>
+            <div class="card align-items-center text-center" style="width: 18rem;">
+                <img src="http://via.placeholder.com/460" class="card-img-top" alt="photo 1">
+                <div class="card-body">
+                    <h5 class="card-title">Minguito Tavares, 12 años</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur obcaecati laboriosam debitis, deleniti delectus suscipit enim inventore possimus neque officiis alias vel quaerat rerum, ipsa esse ab expedita molestias accusamus!</p>
+                    <a href="localizacion.html#card" class="btn btn-primary">Solicita un Evangelio</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
